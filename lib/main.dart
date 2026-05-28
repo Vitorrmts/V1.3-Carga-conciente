34868a6d21a2d353c12ebb5bb76f9ace63d1557c
Vitor1xx <vitormartinssantossilva@gmail.com>
2026-05-28 14:38:00 -0300

    Update main.dart
    Refactor CargaConscienteApp for improved readability and styling.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+
 import 'pages/home_page.dart';
 
 void main() {
-  runApp(const CargaConscienteApp());
+
+  runApp(
+    const CargaConscienteApp(),
+  );
 }
 
-class CargaConscienteApp extends StatelessWidget {
-  const CargaConscienteApp({super.key});
+class CargaConscienteApp
+    extends StatelessWidget {
+
+  const CargaConscienteApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+
+      debugShowCheckedModeBanner:
+          false,
+
       title: 'CargaConsciente',
-      theme: ThemeData.dark(),
+
+      theme: ThemeData.dark().copyWith(
+
+        scaffoldBackgroundColor:
+            const Color(0xFF121212),
+
+        appBarTheme:
+            const AppBarTheme(
+          backgroundColor:
+              Colors.green,
+        ),
+      ),
+
       home: const HomePage(),
     );
   }
