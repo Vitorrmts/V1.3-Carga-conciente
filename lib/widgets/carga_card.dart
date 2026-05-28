6272e2741a67c9ac2ef52616b9c180c1f0464ee4
Vitor1xx <vitormartinssantossilva@gmail.com>
2026-05-28 14:37:25 -0300

    Update carga_card.dart
    Updated CargaCard widget to include a leading icon and changed card color.

--- lib/widgets/carga_card.dart
+++ lib/widgets/carga_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+
 import '../classes/carga.dart';
 
 class CargaCard extends StatelessWidget {
+
   final Carga carga;
 
   const CargaCard({
@@ -11,22 +13,43 @@ class CargaCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return Card(
-      color: Colors.grey[900],
+
+      color: const Color(0xFF1E1E1E),
+
       child: ListTile(
+
+        leading: const Icon(
+          Icons.bolt,
+          color: Colors.green,
+        ),
+
         title: Text(
           carga.mes,
-          style: const TextStyle(color: Colors.white),
-        ),
-        subtitle: Text(
-          '${carga.kwh} kWh',
-          style: const TextStyle(color: Colors.white70),
-        ),
-        trailing: Text(
-          'R\$ ${carga.valor.toStringAsFixed(2)}',
+
           style: const TextStyle(
-            color: Colors.greenAccent,
-            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+
+        subtitle: Text(
+
+          '${carga.kwh} kWh',
+
+          style: const TextStyle(
+            color: Colors.white70,
+          ),
+        ),
+
+        trailing: Text(
+
+          'R\$ ${carga.valor.toStringAsFixed(2)}',
+
+          style: const TextStyle(
+            color: Colors.green,
+            fontWeight:
+                FontWeight.bold,
           ),
         ),
       ),
