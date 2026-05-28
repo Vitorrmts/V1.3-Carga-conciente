5567d98ca987d186f436b3313fba39362c442557
Vitor1xx <vitormartinssantossilva@gmail.com>
2026-05-28 14:35:51 -0300

    Update carga.dart

--- lib/classes/carga.dart
+++ lib/classes/carga.dart
@@ -1,11 +1,36 @@
 class Carga {
+
+  int? id;
+
   final String mes;
   final double kwh;
   final double valor;
 
   Carga({
+    this.id,
     required this.mes,
     required this.kwh,
     required this.valor,
   });
+
+  Map<String, dynamic> toMap() {
+
+    return {
+      'id': id,
+      'mes': mes,
+      'kwh': kwh,
+      'valor': valor,
+    };
+  }
+
+  factory Carga.fromMap(
+      Map<String, dynamic> map) {
+
+    return Carga(
+      id: map['id'],
+      mes: map['mes'],
+      kwh: map['kwh'],
+      valor: map['valor'],
+    );
+  }
 }
